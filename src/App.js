4794bace3535539ed2b685c7ea742b9a3630d94a
Ali <aliyoussef34@hotmail.fr>
2022-02-01 05:20:43 +0100

    stable with react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [messageContent, setMessageContent] = useState("");
   const handleClick = () => {
     // eslint-disable-next-line no-undef
     chrome.runtime.sendMessage({
       msg: "startAutoSend",
     });
   };
+
+  const handleChange = (event) => {
+    setMessageContent(event.target.value);
+  };
   return (
     <div className="App">
-      <button onClick={handleClick}>Start</button>
+      <div className="header">
+        <span className="headerText">Vinted Auto Responder</span>
+      </div>
+      <div className="body">
+        <div className="inputContainer">
+          <input
+            label="Contenu du message"
+            onChange={handleChange}
+            value={messageContent}
+            className="input"
+          />
+        </div>
+        <button className="startSendButton" onClick={handleClick}>
+          <span className="buttonText">Start Sending</span>
+        </button>
+      </div>
     </div>
   );
 }
