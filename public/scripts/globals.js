fa90a6a9004a95244285e930c765e2cfa73fd437
Ali <aliyoussef34@hotmail.fr>
2022-02-06 03:26:19 +0100

    restructured with global functions

---
+++ public/scripts/globals.js
@@ -0,0 +1,128 @@
+// eslint-disable-next-line no-undef
+globalThis.asyncForEach = async function (array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+};
+
+// eslint-disable-next-line no-undef
+globalThis.saveToStoragePromise = async function (key, value) {
+  return new Promise((resolve) => {
+    // eslint-disable-next-line no-undef
+    chrome.storage.local.set({ [key]: value }, resolve);
+  });
+};
+
+// eslint-disable-next-line no-undef
+globalThis.fetchAllNotifications = async function () {
+  try {
+    const notificationsUrl = `https://www.vinted.fr/api/v2/notifications`;
+    const stream = await fetch(notificationsUrl);
+    const jsonData = await stream.json();
+    const allNotifications = jsonData.notifications;
+    return allNotifications;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// eslint-disable-next-line no-undef
+globalThis.extractNotifSenderUserId = function (notification) {
+  const { body } = notification;
+  const bodyUrl = body.match(/\bhttps?:\/\/\S+/gi)[0];
+  const notifSenderId = bodyUrl.replace(/\D/g, ""); // filter recipient id from url
+  return parseInt(notifSenderId);
+};
+
+// eslint-disable-next-line no-undef
+globalThis.getMsgThreadId = async function ({ itemId, msgRecipientId }) {
+  const res = await fetch(
+    `https://www.vinted.fr/items/${itemId}/want_it/new?offering_id=${msgRecipientId}`
+  );
+  const inboxUrl = res.url;
+  const threadId = inboxUrl.replace(/\D/g, "");
+  return threadId;
+};
+
+// eslint-disable-next-line no-undef
+globalThis.sendMessageByQuery = async function ({
+  currentUserId,
+  msgThreadId,
+  messageContent,
+  csrf_token,
+}) {
+  await fetch(
+    `https://www.vinted.fr/api/v2/users/${currentUserId}/msg_threads/${msgThreadId}`,
+    {
+      headers: {
+        accept: "application/json, text/plain, */*",
+        "accept-language": "fr",
+        "content-type": "application/json",
+        "sec-ch-ua": '"Chromium";v="96", "Opera";v="82", ";Not A Brand";v="99"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-csrf-token": `${csrf_token}`,
+      },
+      referrer: `https://www.vinted.fr/inbox/${msgThreadId}`,
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: `{"body":"${messageContent}","photo_temp_uuids":null}`,
+      method: "PUT",
+      mode: "cors",
+      credentials: "include",
+    }
+  )
+    .then(async (response) => {
+      return await response.json();
+    })
+    .then((data) => {
+      console.log("Sent message");
+      return data;
+    })
+    .catch((err) => {
+      console.log("error when sending message", err);
+    });
+};
+
+// eslint-disable-next-line no-undef
+globalThis.deleteMessageThreadId = async function ({
+  currentUserId,
+  msgThreadId,
+  csrf_token,
+}) {
+  await fetch(
+    `https://www.vinted.fr/api/v2/users/${currentUserId}/msg_threads/${msgThreadId}`,
+    {
+      headers: {
+        accept: "application/json, text/plain, */*",
+        "accept-language": "fr",
+        "content-type": "application/json",
+        "sec-ch-ua": '"Chromium";v="96", "Opera";v="82", ";Not A Brand";v="99"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-csrf-token": `${csrf_token}`,
+      },
+      referrer: `https://www.vinted.fr/inbox/${msgThreadId}`,
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: null,
+      method: "DELETE",
+      mode: "cors",
+      credentials: "include",
+    }
+  )
+    .then(async (response) => {
+      return await response.json();
+    })
+    .then((data) => {
+      console.log("Deleted conversation");
+      return data;
+    })
+    .catch((err) => {
+      console.log("Error when deleting message", err);
+    });
+};
--- public/scripts/sendMessageToAllFavers/index.js
+++ public/scripts/sendMessageToAllFavers/index.js
@@ -2,134 +2,12 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-undef */
 
-async function fetchAllNotifications() {
-  try {
-    const notificationsUrl = `https://www.vinted.fr/api/v2/notifications`;
-    const stream = await fetch(notificationsUrl);
-    const jsonData = await stream.json();
-    const allNotifications = jsonData.notifications;
-    return allNotifications;
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
-}
-
-function extractNotifSenderUserId(notification) {
-  const { body } = notification;
-  const bodyUrl = body.match(/\bhttps?:\/\/\S+/gi)[0];
-  const notifSenderId = bodyUrl.replace(/\D/g, ""); // filter recipient id from url
-  return parseInt(notifSenderId);
-}
-
-async function getMsgThreadId({ itemId, msgRecipientId }) {
-  const res = await fetch(
-    `https://www.vinted.fr/items/${itemId}/want_it/new?offering_id=${msgRecipientId}`
-  );
-  const inboxUrl = res.url;
-  const threadId = inboxUrl.replace(/\D/g, "");
-  return threadId;
-}
-
-async function sendMessageByQuery({
-  currentUserId,
-  msgThreadId,
-  messageContent,
-  csrf_token,
-}) {
-  await fetch(
-    `https://www.vinted.fr/api/v2/users/${currentUserId}/msg_threads/${msgThreadId}`,
-    {
-      headers: {
-        accept: "application/json, text/plain, */*",
-        "accept-language": "fr",
-        "content-type": "application/json",
-        "sec-ch-ua": '"Chromium";v="96", "Opera";v="82", ";Not A Brand";v="99"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"macOS"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "x-csrf-token": `${csrf_token}`,
-      },
-      referrer: `https://www.vinted.fr/inbox/${msgThreadId}`,
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: `{"body":"${messageContent}","photo_temp_uuids":null}`,
-      method: "PUT",
-      mode: "cors",
-      credentials: "include",
-    }
-  )
-    .then(async (response) => {
-      return await response.json();
-    })
-    .then((data) => {
-      console.log("Sent message");
-      return data;
-    })
-    .catch((err) => {
-      console.log("error when sending message", err);
-    });
-}
-
-async function deleteMessageThreadId({
-  currentUserId,
-  msgThreadId,
-  csrf_token,
-}) {
-  await fetch(
-    `https://www.vinted.fr/api/v2/users/${currentUserId}/msg_threads/${msgThreadId}`,
-    {
-      headers: {
-        accept: "application/json, text/plain, */*",
-        "accept-language": "fr",
-        "content-type": "application/json",
-        "sec-ch-ua": '"Chromium";v="96", "Opera";v="82", ";Not A Brand";v="99"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"macOS"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "x-csrf-token": `${csrf_token}`,
-      },
-      referrer: `https://www.vinted.fr/inbox/${msgThreadId}`,
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: null,
-      method: "DELETE",
-      mode: "cors",
-      credentials: "include",
-    }
-  )
-    .then(async (response) => {
-      return await response.json();
-    })
-    .then((data) => {
-      console.log("Deleted conversation");
-      return data;
-    })
-    .catch((err) => {
-      console.log("Error when deleting message", err);
-    });
-}
-
-async function saveToStoragePromise(key, value) {
-  return new Promise((resolve) => {
-    chrome.storage.local.set({ [key]: value }, resolve);
-  });
-}
-// -------------------------------------------
-
 async function sendMessageToAllFavers({
   lastNotificationHandled,
   messageContent,
   deleteEachConvo,
 }) {
-  const allNotifications = await fetchAllNotifications();
+  const allNotifications = await globalThis.fetchAllNotifications();
   console.log("deleteEachConvo");
   console.log(deleteEachConvo);
 
@@ -170,7 +48,7 @@ async function sendMessageToAllFavers({
 
   unreadFavNotifications = unreadFavNotifications.map((notif) => ({
     ...notif,
-    senderUserId: extractNotifSenderUserId(notif),
+    senderUserId: globalThis.extractNotifSenderUserId(notif),
   }));
 
   const currentUserId = unreadFavNotifications[0].user_id;
@@ -178,13 +56,13 @@ async function sendMessageToAllFavers({
   const csrf_token = document.head.querySelector("[name=csrf-token]").content;
   if (csrf_token) {
     let tempLastNotifHandledId;
-    await asyncForEach(unreadFavNotifications, async (notif) => {
-      const msgThreadId = await getMsgThreadId({
+    await globalThis.asyncForEach(unreadFavNotifications, async (notif) => {
+      const msgThreadId = await globalThis.getMsgThreadId({
         itemId: notif.subject_id,
         msgRecipientId: notif.senderUserId,
       });
 
-      await sendMessageByQuery({
+      await globalThis.sendMessageByQuery({
         currentUserId: currentUserId,
         msgThreadId: msgThreadId,
         csrf_token: csrf_token,
@@ -192,7 +70,7 @@ async function sendMessageToAllFavers({
       });
 
       if (deleteEachConvo) {
-        await deleteMessageThreadId({
+        await globalThis.deleteMessageThreadId({
           currentUserId: currentUserId,
           msgThreadId: msgThreadId,
           csrf_token: csrf_token,
@@ -201,7 +79,7 @@ async function sendMessageToAllFavers({
 
       tempLastNotifHandledId = notif.id;
     });
-    await saveToStoragePromise(
+    await globalThis.saveToStoragePromise(
       "lastNotificationHandled",
       tempLastNotifHandledId
     );
